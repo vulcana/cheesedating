15920060e4c9bf52f8dad5a578f5d0ef37ec1c15
margaritakop <mbk11@ic.ac.uk>
2016-11-07 15:32:58 +0000

    Spin when on Make button and yellow selection
    next: make spinnign just stop without returning to original position.

--- scripts.js
+++ scripts.js
@@ -17,64 +17,76 @@ $(function() {
 	$('.cheese_recipe').hide()
 });
 
-//select the selected cheeses by adding "selected" calss
+//select the recipes corresponding to selected cheeses by adding "selected" calss
+//also add the border properties to stay
 $(function() {
 $("#halloumi_cheese").click(function(){
     $(".halloumi_recipe" ).addClass("selected");
-    $("#halloumi_cheese").css({"border-color":"#337ab7"});
+    $("#halloumi_cheese").css({"border-color":"#ffd500"});
+    $("#halloumi_cheese").css({"border-width":"2px"});
             });
 });
-
 $(function() {
 $("#gorgonzola_cheese").click(function(){
     $(".gorgonzola_recipe" ).addClass("selected" );
-    $("#gorgonzola_cheese").css({"border-color":"#337ab7"});
+    $("#gorgonzola_cheese").css({"border-color":"#ffd500"});
+    $("#gorgonzola_cheese").css({"border-width":"2px"});
             });
 }); 
 $(function() {
 $("#swiss_cheese").click(function(){
     $(".swiss_cheese_recipe" ).addClass("selected" );
-    $("#swiss_cheese").css({"border-color":"#337ab7"});
+    $("#swiss_cheese").css({"border-color":"#ffd500"});
+    $("#swiss_cheese").css({"border-width":"2px"});
             });
 }); 
 $(function() {
 $("#camembert").click(function(){
     $(".camembert_recipe" ).addClass("selected" );
-    $("#camembert").css({"border-color":"#337ab7"});
+    $("#camembert").css({"border-color":"#ffd500"});
+    $("#camembert").css({"border-width":"2px"});
             });
 }); 
 $(function() {
 $("#red_leicester").click(function(){
     $(".red_leicester_recipe" ).addClass("selected" );
-    $("#red_leicester").css({"border-color":"#337ab7"});
+    $("#red_leicester").css({"border-color":"#ffd500"});
+    $("#red_leicester").css({"border-width":"2px"});
             });
 }); 
 $(function() {
 $("#cottage_cheese").click(function(){
     $(".cottage_cheese_recipe" ).addClass("selected" );
-    $("#cottage_cheese").css({"border-color":"#337ab7"});
+    $("#cottage_cheese").css({"border-color":"#ffd500"});
+    $("#cottage_cheese").css({"border-width":"2px"});
             });
 }); 
 
+//Make mea meal!
 $(function() {
 $("#make_button").click(function(){
     $(".selected").show();
 });
 }); 
+
+//cheeseboard spins when mouse is on on the makemeal button area too.
 $(function() {
-$("#reset").click(function(){
-    $(".recipe").hide();
-  });
+$("#make_button").hover(function(){
+$("#cheeseboard").addClass("spin" );
+}); 
+}); 
+$(function() {
+$("#make_button").mouseleave(function() {
+  $("#cheeseboard").removeClass("spin" );
+}); 
 }); 
 
+//reset button
 $(function() {
 $("#reset").click(function(){
     $(".cheese_recipe").hide();
     $(".cheese_recipe").removeClass("selected");
     $(".thumbnail").css({"border-color":"transparent"});
+    $("#cheeseboard").removeClass("spin" );
 }); 
 }); 
-
-//$(function() {
-//$("#make_button").hover(function(){
-//$("#cheeseboard").spin!! I don`t know how to do that. adding hover class doen`t work.
